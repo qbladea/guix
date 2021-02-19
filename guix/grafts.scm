a5122c8e380825e99142036e34eedfb38a93ced2
Ludovic Courtès <ludo@gnu.org>
2021-02-19 22:19:41 +0100

    grafts: Inline 'grafting?'.
    As for 'current-target-system' & co., this makes sure we don't
    needlessly allocate closures.
    
    * guix/grafts.scm (grafting?): Inline.

--- guix/grafts.scm
+++ guix/grafts.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -329,8 +329,8 @@ it otherwise.  It returns the previous setting."
   (lambda (store)
     (values (%graft? enable?) store)))
 
-(define (grafting?)
-  "Return a Boolean indicating whether grafting is enabled."
+(define-inlinable (grafting?)
+  ;; Return a Boolean indicating whether grafting is enabled.
   (lambda (store)
     (values (%graft?) store)))
 
